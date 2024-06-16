4968ca72da213800e15d6889bda8848f2d50e9ec
Samreen Abdul Razzaq <samreenabdulrazzaq6167@gmil.com>
2024-06-16 22:29:41 +0500

    create Q7

---
+++ src/Q7.ts
@@ -0,0 +1,13 @@
+//Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let a :number = 5
+let b : number = 3
+console.log("Addition" ,a + b)
+let c : number = 9
+let d : number = 1
+console.log("Subtraction" , c - d);
+let e : number = 2
+let f : number = 4
+console.log("Multiplication", e * f);
+let g : number = 16
+let h : number = 2
+console.log("Divison", g/h);
