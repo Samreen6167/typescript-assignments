64ac4500698fffb1ffeb84286cbfd10dc7e4e222
Samreen Abdul Razzaq <samreenabdulrazzaq6167@gmil.com>
2024-06-16 22:14:08 +0500

    create numbereight.ts

---
+++ dist/favno.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favouriteNumber = 16;
+let message = `My Favourite Number is :  "${favouriteNumber}"`;
+console.log(message);
---
+++ dist/numbereight.js
@@ -0,0 +1,14 @@
+"use strict";
+//Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let a = 5;
+let b = 3;
+console.log("Addition", a + b);
+let c = 9;
+let d = 1;
+console.log("Subtraction", c - d);
+let e = 2;
+let f = 4;
+console.log("Multiplication", e * f);
+let g = 16;
+let h = 2;
+console.log("Divison", g / h);
