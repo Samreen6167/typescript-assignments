c9f02c778a225c04119743d2f68f2446d878677e
Samreen Abdul Razzaq <samreenabdulrazzaq6167@gmil.com>
2024-06-16 22:35:35 +0500

    create Q9.ts

---
+++ src/Q9.ts
@@ -0,0 +1,6 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favouriteNumber : number = 16;
+let message = `My Favourite Number is :  "${favouriteNumber}"`;
+console.log(message);
+
